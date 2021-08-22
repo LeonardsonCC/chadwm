d63107ae41edfdc44e78cc0ae6492c28647f4f4e
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-08-21 23:34:21 -0300

    Kitty as my default terminal

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -138,7 +138,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
-static const char *termcmd[]  = {  "terminator", NULL }; // change this to your term
+static const char *termcmd[]  = {  "kitty", NULL }; // change this to your term
 static const char *rofi[] = {"rofi", "-show", "drun", NULL };
 static const char *layoutmenu_cmd = "/home/leonardson/.dwm/layoutmenu.sh";
 static const char *xi[] = {"xbacklight", "-inc", "7", NULL};
